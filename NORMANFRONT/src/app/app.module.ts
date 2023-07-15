f93fe0d19a2d12907e96f9a698814ed5221ddb77
Pia <piaqasalas@gmail.com>
2023-07-14 23:11:52 -0300

    arreglos

--- NORMANFRONT/src/app/app.module.ts
+++ NORMANFRONT/src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { QuienessomosComponent } from './components/quienessomos/quienessomos.component';
+import { StaffComponent } from './components/staff/staff.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { BotonesComponent } from './components/botones/botones.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    EventosComponent,
+    QuienessomosComponent,
+    StaffComponent,
+    ContactoComponent,
+    BotonesComponent
   ],
   imports: [
     BrowserModule,
