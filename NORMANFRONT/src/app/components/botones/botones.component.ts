0817c1efd09ad7ea858e2cd0e5fc9bb457f4c0c3
Pia <piaqasalas@gmail.com>
2023-07-16 18:13:16 -0300

    adding login

--- NORMANFRONT/src/app/components/botones/botones.component.ts
+++ NORMANFRONT/src/app/components/botones/botones.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { StaffComponent } from 'C:/Users/Hp/Desktop/NORMANFRONT/NORMANFRONT/src/app/components/staff/staff.component';
 
 @Component({
   selector: 'app-botones',
   templateUrl: './botones.component.html',
   styleUrls: ['./botones.component.css']
+  
 })
-export class BotonesComponent {
+export class BotonesComponent {}
 
-}
--- NORMANFRONT/src/app/components/staff/staff.component.ts
+++ NORMANFRONT/src/app/components/staff/staff.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-staff',
   templateUrl: './staff.component.html',
   styleUrls: ['./staff.component.css']
 })
-export class StaffComponent {
+export class StaffComponent implements OnInit {
+  ngOnInit() {
+    const signUpButton = document.getElementById('signUp') as HTMLButtonElement;
+    const signInButton = document.getElementById('signIn') as HTMLButtonElement;
+    const container = document.getElementById('container') as HTMLDivElement;
 
+    signUpButton.addEventListener('click', () => {
+      container.classList.add('right-panel-active');
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove('right-panel-active');
+    });
+  }
 }
